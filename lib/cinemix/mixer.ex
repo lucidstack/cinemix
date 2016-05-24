36ba3a48e523e4bd1a4988544088097cc42ecc80
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-05-25 00:56:32 +0100

    handle error in marky_markov in Mixer

--- lib/cinemix/mixer.ex
+++ lib/cinemix/mixer.ex
@@ -1,4 +1,6 @@
 defmodule Cinemix.Mixer do
+  import Logger
+
   def mix(plots) when is_list(plots) do
     mix(plots |> Enum.join(" "))
   end
@@ -7,8 +9,13 @@ defmodule Cinemix.Mixer do
     tmp_path = "/tmp/cinemix_#{:rand.uniform(1000)}"
 
     File.write(tmp_path, plot)
-    {mixed, 0} = System.cmd("marky_markov",
-                       ["speak", "-s", tmp_path, "-c", "8"])
+    mixed = case System.cmd("marky_markov", ["speak", "-s", tmp_path, "-c", "8"]) do
+      {mixed, 0} ->
+        mixed
+      {_,  code} ->
+        warn("code #{code} from marky. tmp_path is #{tmp_path}")
+        ""
+    end
 
     File.rm(tmp_path)
     mixed
