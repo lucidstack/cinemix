37da3be0b705128bb4258bd58137688bf51e3b9f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-05-25 00:57:02 +0100

    bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cinemix.Mixfile do
 
   def project do
     [app: :cinemix,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
